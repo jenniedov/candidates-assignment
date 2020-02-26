220e15d65739ed8e23b692a163af85c4057db228
jennie.dobrovolsky@chorus.ai <jennie.dobrovolsky@chorus.ai>
2020-02-27 00:22:40 +0200

    Added fixes for all other exercises

--- vagrant/fixScripts/exercise4-fix_server2.sh
+++ vagrant/fixScripts/exercise4-fix_server2.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 #add fix to exercise4-server2 here
+sudo  -- sh -c "echo '192.168.100.10 server1' >> /etc/hosts"
+sudo  -- sh -c "echo '192.168.100.10 other_server' >> /etc/hosts"
--- vagrant/fixScripts/exercise5-fix_server2.sh
+++ vagrant/fixScripts/exercise5-fix_server2.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
 #add fix to exercise5-server2 here
+ssh-keygen -b 2048 -t rsa -f /home/vagrant/.ssh/id_rsa -q -N ""
+
+sudo chown vagrant /home/vagrant/.ssh/id_rsa /home/vagrant/.ssh/id_rsa.pub
+cat /vagrant/id_rsa.pub >> /home/vagrant/.ssh/authorized_keys
+sudo apt-get install -y sshpass
+
+echo """Host server1
+    HostName server1
+    User vagrant
+    StrictHostKeyChecking no
+
+Host other_server
+    HostName other_server
+    User vagrant
+    StrictHostKeyChecking no
+""" > /home/vagrant/.ssh/config
+
+sudo -u vagrant sshpass -p vagrant ssh-copy-id -i /home/vagrant/.ssh/id_rsa.pub vagrant@server1
